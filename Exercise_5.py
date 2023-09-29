1930639a09c6e46a2aef8b32ef7ff59d393c5678
Alex <alex192003@bk.ru>
2023-09-29 11:12:23 +0500

    Edited Exersice_5

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,4 +1,12 @@
-for i in range(4):
-    print("hello world", "hello", sep="\n")
-    if i == 3:
-        print("hello world")
+counter = 0
+string = 'hello'
+values = [0,2,4,6,8,10]
+while counter != 10:
+    memory = string
+    if counter in values:
+        string = string + ' world'
+    print(string)
+    string = memory
+    counter+=1
+memory = ' world'
+print(string + memory)
